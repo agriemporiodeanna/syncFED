c33e9e6090560e324ee750bd1cefeb5ad8cea864
agriemporiodeanna <shop@agriemporiodeanna.com>
2025-12-11 12:27:37 +0100

    Delete ftp.js

--- ftp.js
+++
@@ -1,8 +0,0 @@
-// Modulo FTP semplificato: per ora uploadImage è solo uno stub
-// così il resto dell'applicazione non va in errore.
-
-export async function uploadImage(localPath, remoteFileName) {
-  console.log("📁 [FAKE FTP] uploadImage", { localPath, remoteFileName });
-  // Restituiamo un path simbolico
-  return `/img/${remoteFileName}`;
-}
